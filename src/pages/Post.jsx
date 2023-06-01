10cec8d69d5ad9724d8126891d55807433664b71
eren <erenkuek1106@gmail.com>
2023-06-01 11:11:26 +0800

    Done edit function and image slides logic

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/post.module.css';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import PlaceIcon from '@mui/icons-material/Place';
@@ -7,95 +7,144 @@ import SendIcon from '@mui/icons-material/Send';
 import Comment from '../components/Comment';
 
 const Post = () => {
+  const [showOption, setShowOption] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [postData, setPostData] = useState({
+    description: "Japan is a fascinating destination that seamlessly blends old-world charm with modern innovation. From the bustling streets of Tokyo to the peaceful temples of Kyoto, there's something for every traveler in this incredible country. Indulge in delicious cuisine, witness breathtaking natural beauty, and immerse yourself in the unique culture and traditions of Japan.",
+    location: "Japan, Tokyo",
+    images: [
+      `${process.env.PUBLIC_URL}/assets/japanView.jpeg`,
+      `${process.env.PUBLIC_URL}/assets/balloonView.jpg`,
+      `${process.env.PUBLIC_URL}/assets/usaView.jpg`
+    ],
+  })
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   const toggleShowOption = () => {
-    // Add your toggleShowOption logic here
+    setShowOption(!showOption);
   };
 
   const showEdit = () => {
-    // Add your showEdit logic here
-  };
-
-  const showDelete = () => {
-    // Add your showDelete logic here
-  };
-
-  const saveEdit = () => {
-    // Add your saveEdit logic here
+    setShowOption(false);
+    setIsEdit(true);
   };
 
   const closeEdit = () => {
-    // Add your closeEdit logic here
+    setIsEdit(false);
   };
 
-  const plusSlides = (n) => {
-    // Add your plusSlides logic here
-  };
-
-  const currentSlide = (n) => {
-    // Add your currentSlide logic here
-  };
-
-  const deletePost = () => {
-    // Add your deletePost logic here
+  const showDelete = () => {
+    setShowOption(false);
+    setShowDeleteAlert(true);
   };
 
   const closeDelete = () => {
-    // Add your closeDelete logic here
+    setShowDeleteAlert(false);
   };
+
+  const saveEdit = () => {
+    setIsEdit(false);
+  };
+
+  const plusSlides = (n) => {
+    let index = currentImageIndex + n;
+    const len = postData.images.length
+    if (index > len - 1) index = 0;
+    else if (index < 0) index = len - 1;
+    setCurrentImageIndex(index);
+  };
+
+  const selectSlide = (n) => {
+    setCurrentImageIndex(n);
+  };
+
+  const deletePost = () => {
+    setShowDeleteAlert(false);
+    alert("Post deleted");
+  };
+
+  const handleDescriptionChange = (e) => {
+    setPostData({
+      ...postData,
+      description: e.target.value,
+    });
+  }
+
+  const handleLocationChange = (e) => {
+    setPostData({
+      ...postData,
+      location: e.target.value,
+    });
+  }
 
   return (
     <main className={styles.postWrapper}>
       <button className={`${styles.purpleBtn} ${styles.backBtn}`}>
         Back
       </button>
-      <div className={`${styles.postPanel}`}>
+      <div className={styles.postPanel}>
         <button className={`${styles.optionBtn} ${styles.iconWrapper}`} onClick={toggleShowOption}>
           <MoreHorizIcon />
         </button>
-        <ul className={`${styles.optionsList}`}>
-          <li className={`${styles.option}`} onClick={showEdit}>Edit post</li>
-          <li className={`${styles.option}`} onClick={showDelete}>Delete post</li>
-        </ul>
-        <section className={`${styles.topSection}`}>
-          <img className={`${styles.profilePic}`} src={`${process.env.PUBLIC_URL}/assets/eren.jpg`} alt="profile pic" />
-          <div className={`${styles.topInfo}`}>
-            <h2 className={`${styles.userName}`}>Eren</h2>
-            <p className={`${styles.postDate}`}>April 9 2023, 13:40 pm</p>
+        {showOption && <ul className={`${styles.optionsList}`}>
+          <li className={styles.option} onClick={showEdit}>Edit post</li>
+          <li className={styles.option} onClick={showDelete}>Delete post</li>
+        </ul>}
+        <section className={styles.topSection}>
+          <img className={styles.profilePic} src={`${process.env.PUBLIC_URL}/assets/eren.jpg`} alt="profile pic" />
+          <div className={styles.topInfo}>
+            <h2 className={styles.userName}>Eren</h2>
+            <p className={styles.postDate}>April 9 2023, 13:40 pm</p>
           </div>
         </section>
-        <section className={`${styles.contentSection}`}>
-          <p className={`${styles.caption}`}>Japan is a fascinating destination that seamlessly blends old-world charm with modern innovation. From the bustling streets of Tokyo to the peaceful temples of Kyoto, there's something for every traveler in this incredible country. Indulge in delicious cuisine, witness breathtaking natural beauty, and immerse yourself in the unique culture and traditions of Japan.</p>
-          <textarea className={`${styles.captionInput}`} type="text" placeholder="Write caption..." rows="5"></textarea>
-          <div className={`${styles.locationTag}`}>
-            <PlaceIcon fontSize='small'/>
-            <p className={`${styles.locationText}`}>Tokyo, Japan</p>
-            <input className={`${styles.locationInput}`} type="text" />
+        <section className={styles.contentSection}>
+          {isEdit ?
+            <textarea
+              value={postData.description}
+              className={styles.captionInput}
+              type="text"
+              placeholder="Write caption..."
+              rows="5"
+              onChange={handleDescriptionChange}
+            />
+            : <p className={styles.caption}>{postData.description}</p>
+          }
+          <div className={styles.locationTag}>
+            <PlaceIcon fontSize='small' />
+            {
+              isEdit ? <input
+                value={postData.location}
+                className={styles.locationInput}
+                type="text"
+                onChange={handleLocationChange}
+              />
+                : <p className={styles.locationText}>{postData.location}</p>
+            }
           </div>
-          <div className={`${styles.editBar}`}>
+          {isEdit && <div className={styles.editBar}>
             <button className={`${styles.saveBtn} ${styles.editBtn} ${styles.purpleBtn}`} onClick={saveEdit}>Save</button>
             <button className={`${styles.cancelBtn} ${styles.editBtn} ${styles.blueBtn}`} onClick={closeEdit}>Cancel</button>
-          </div>
-          <div className={`${styles.slideshowContainer}`}>
-            <div className={`${styles.fade}`}>
-              <div className={styles.numbertext}>1 / 3</div>
-              <img className={styles.slideImg} src="assets/japanView.jpeg" alt="Slide 1" />
-            </div>
-            <div className={`${styles.hideSlide} ${styles.fade}`}>
-              <div className={styles.numbertext}>2 / 3</div>
-              <img className={styles.slideImg} src="assets/balloonView.jpg" alt="Slide 2" />
-            </div>
-            <div className={`${styles.hideSlide} ${styles.fade}`}>
-              <div className={styles.numbertext}>3 / 3</div>
-              <img className={styles.slideImg} src="assets/usaView.jpg" alt="Slide 3" />
-            </div>
-            <a className={styles.prev} onClick={() => plusSlides(-1)}>❮</a>
-            <a className={styles.next} onClick={() => plusSlides(1)}>❯</a>
+          </div>}
+          <div className={styles.slideshowContainer}>
+            {
+              postData.images.map((image, index) =>
+                <div className={`${currentImageIndex !== index && styles.hideSlide} ${styles.fade}`}>
+                  <div className={styles.numbertext}>{`${index + 1}/${postData.images.length}`}</div>
+                  <img className={styles.slideImg} src={image} alt="post" />
+                </div>
+              )
+            }
+            <div className={styles.prev} onClick={() => plusSlides(-1)}>❮</div>
+            <div className={styles.next} onClick={() => plusSlides(1)}>❯</div>
           </div>
           <br />
           <div style={{ textAlign: 'center' }}>
-            <span className={styles.dot} onClick={() => currentSlide(1)}></span>
-            <span className={styles.dot} onClick={() => currentSlide(2)}></span>
-            <span className={styles.dot} onClick={() => currentSlide(3)}></span>
+            {
+              postData.images.map((image, index) =>
+                <span className={styles.dot} onClick={() => selectSlide(index)}></span>
+              )
+            }
           </div>
           <div className={`${styles.ratingAndComments}`}>
             <p className={`${styles.ratingText} ${styles.dataText}`}>4.5 average rating</p>
@@ -108,11 +157,11 @@ const Post = () => {
           <div className={`${styles.ratingWrapper}`}>
             <p>Give a rating:</p>
             <div className={`${styles.ratingStars}`}>
-              <StarIcon className={styles.ratingStar}/>
-              <StarIcon className={styles.ratingStar}/>
-              <StarIcon className={styles.ratingStar}/>
-              <StarIcon className={styles.ratingStar}/>
-              <StarIcon className={styles.ratingStar}/>
+              <StarIcon className={styles.ratingStar} />
+              <StarIcon className={styles.ratingStar} />
+              <StarIcon className={styles.ratingStar} />
+              <StarIcon className={styles.ratingStar} />
+              <StarIcon className={styles.ratingStar} />
             </div>
             <button className={`${styles.clearBtn} ${styles.purpleBtn}`}>Clear Rating</button>
           </div>
@@ -141,15 +190,17 @@ const Post = () => {
             </button>
           </div>
         </section>
-        <div className={`${styles.confirmModal} ${styles.popUpModal}`}>
-          <div className={`${styles.modalContent}`}>
-            <p className={`${styles.modalText}`}>Are you sure you want to delete this post?</p>
-            <div className={`${styles.modalButtons}`}>
-              <button className={`${styles.confirmDeleteBtn} ${styles.modalBtn} ${styles.purpleBtn}`} onClick={deletePost}>Yes</button>
-              <button className={`${styles.cancelDeleteBtn} cancelDeleteBtn ${styles.modalBtn} ${styles.blueBtn}`} onClick={closeDelete}>No</button>
+        {
+          showDeleteAlert && <div className={`${styles.confirmModal} ${styles.popUpModal}`}>
+            <div className={`${styles.modalContent}`}>
+              <p className={`${styles.modalText}`}>Are you sure you want to delete this post?</p>
+              <div className={`${styles.modalButtons}`}>
+                <button className={`${styles.confirmDeleteBtn} ${styles.modalBtn} ${styles.purpleBtn}`} onClick={deletePost}>Yes</button>
+                <button className={`${styles.cancelDeleteBtn} cancelDeleteBtn ${styles.modalBtn} ${styles.blueBtn}`} onClick={closeDelete}>No</button>
+              </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     </main>
   );
