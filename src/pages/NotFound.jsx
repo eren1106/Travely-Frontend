a982932edbfb80d11110b063b270485e4988dde9
Ryanangry07 <S2022048@siswa.um.edu.my>
2023-06-12 05:52:24 +0800

    404 not found page
    submit 404 not found page first

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from '../styles/notFound.css';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="slider-area"> 
+        <div className="slider-content">
+            <div className="row">
+                <div className="col-md-6 mx-auto text-center">
+                    <img className="img-fluid" src="static/picture/4042.png" alt="" />
+                    <br />
+                    <br />
+                    <h2 className="h2">Oops! </h2>
+                    <p>We're sorry, <br />
+                        The page you were looking for doesn't exist anymore.
+                    </p>
+                    <a className="btn shuvo-btn" href="#"> Back to Home</a>
+                </div> 
+            </div>
+        </div>
+    </div>
   )
 }
 
