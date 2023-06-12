eef617fd5fefb6be9d8ea5538b4c3ab43e09f0c6
Ryanangry07 <S2022048@siswa.um.edu.my>
2023-06-12 16:20:13 +0800

    html and css of login and register
    update first version of login and register function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React from 'react';
+import '../styles/register-login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    
+    console.log('Logged in:', email, password);
+  };
+
   return (
-    <div>Login</div>
+    <div className="container">
+      <div classNameName="forms-container">
+        <div classNameName="signin-signup">
+          <form action="" className="sign-in-form" id="login-form" autocomplete="off">
+            <h2 className="title">Sign in to Travely</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="forget-password-link">
+              <p className="forget-password-link-text">
+                <Link to='/forgetPassword' href="password-reset.html">Forget Password?</Link>
+              </p>
+            </div>
+
+            <input onClick={handleLogin} type="submit" value="Login" className="btn solid" />
+            
+            <p className="social-text">Or Sign in With</p>
+            <div className="social-media">
+              <a href="country.html" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="country.html" className="social-icon">
+                <i className="fab fa-google"></i>
+              </a>
+            </div>
+            <div className="signin-signup-link">
+              <p className="signin-signup-text">
+                Don't have an account? &nbsp;
+                <a href="register.html">Sign up</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="panels-container"></div>
+      <div className="panels-top-container"></div>
+    </div>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import '../styles/register-login.css';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div class="container">
+            <div class="forms-container">
+                <div class="signin-signup sign-up-group">
+                    <form action="" class="" id="register-form" autocomplete="off">
+                        <h2 class="title">Sign up to Travely</h2>
+                        <div class="input-field">
+                            <i class="fas fa-user"></i>
+                            <input type="text" name="username" placeholder="Username" required />
+                        </div>
+                        
+                        <div class="input-field">
+                            <i class="fas fa-user"></i>
+                            <input type="email" name="email" placeholder="Email" required />
+                        </div>
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" name="password" placeholder="Password" required />
+                        </div>
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" name="confirm-password" placeholder="Confirm Password" required />
+                        </div>
+                        <input type="submit" value="Register" class="btn solid" id="sign-up-btn" />
+                        <p class="social-text">Or Sign up With</p>
+                        <div class="social-media">
+                            <a href="#" class="social-icon">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#" class="social-icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                        <div class="signin-signup-link">
+                            <p class="signin-signup-text">
+                                Already have an account? &nbsp;
+                                <a href="login.html">Sign in</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="panels-container"></div>
+            <div class="panels-top-container"></div>
+        </div>
   )
 }
 
