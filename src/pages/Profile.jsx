fd6775768e5656e5c1eacc586aa5d48013fea5bc
26Jon <92423310+26Jon@users.noreply.github.com>
2023-06-16 11:44:50 +0800

    Adjust the positioning of the page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,12 +9,16 @@ const Profile = () => {
   const [profilePic, setProfilePic] = useState('assets/profile.jpg');
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [IsAccountDeleted, setIsAccountDeleted] = useState(false);
+  const [originalUsername, setOriginalUsername] = useState('');
+  const [originalBio, setOriginalBio] = useState('');
 
   // Create refs for DOM elements
   const divUsernameRef = useRef(null);
   const divBioRef = useRef(null);
   const inputUsernameRef = useRef(null);
   const inputBioRef = useRef(null);
+
+  
 
   const handleChangeProfilePic = (event) => {
     const file = event.target.files[0];
@@ -54,6 +58,8 @@ const Profile = () => {
 
     divusername.innerHTML = usernamenew;
     divBio.innerHTML = bionew;
+
+    setIsEditModalOpen(false);
   };
 
   const handleDeleteAccount = () => {
@@ -81,58 +87,129 @@ const Profile = () => {
     });
   };
 
+  /*
+  useLayoutEffect(() => {
+    const inputUsername = inputUsernameRef.current;
+    const inputBio = inputBioRef.current;
+  
+    if (inputUsername && inputBio) {
+      const originalUsername = inputUsername.defaultValue;
+      const originalBio = inputBio.defaultValue;
+  
+      const handleMouseOverUsername = () => {
+        if (inputUsername.value === username) {
+          inputUsername.value = '';
+        }
+      };
+      
+      const handleMouseOverBio = () => {
+        if (inputBio.value === bio) {
+          inputBio.value = '';
+        }
+      };
+      
+      const handleMouseOutUsername = () => {
+        if (inputUsername.value === '') {
+          inputUsername.value = username;
+        }
+      };
+      
+      const handleMouseOutBio = () => {
+        if (inputBio.value === '') {
+          inputBio.value = bio;
+        }
+      };      
+  
+      const handleClickUsername = () => {
+        if (inputUsername.value === originalUsername) {
+          inputUsername.value = '';
+        }
+      };
+  
+      const handleClickBio = () => {
+        if (inputBio.value === originalBio) {
+          inputBio.value = '';
+        }
+      };
+  
+      inputUsername.addEventListener('mouseover', handleMouseOverUsername);
+      inputBio.addEventListener('mouseover', handleMouseOverBio);
+      inputUsername.addEventListener('mouseout', handleMouseOutUsername);
+      inputBio.addEventListener('mouseout', handleMouseOutBio);
+      inputUsername.addEventListener('click', handleClickUsername);
+      inputBio.addEventListener('click', handleClickBio);
+  
+      // Clean up event listeners on component unmount
+      return () => {
+        inputUsername.removeEventListener('mouseover', handleMouseOverUsername);
+        inputBio.removeEventListener('mouseover', handleMouseOverBio);
+        inputUsername.removeEventListener('mouseout', handleMouseOutUsername);
+        inputBio.removeEventListener('mouseout', handleMouseOutBio);
+        inputUsername.removeEventListener('click', handleClickUsername);
+        inputBio.removeEventListener('click', handleClickBio);
+      };
+    }
+  }, []);
+  */
+
+  
   useLayoutEffect(() => {
     displayCurrentUserInfo();
-  }, []);
+  }, [divUsernameRef, divBioRef]);
+  
   
   const displayCurrentUserInfo = () => {
       const divusername = divUsernameRef.current;
       const divBio = divBioRef.current;
 
-      const usernamenew = divusername.innerHTML;
-      const bionew = divBio.innerHTML;
+      if (divusername && divBio) {
+        const usernamenew = divusername.innerHTML;
+        const bionew = divBio.innerHTML;
 
-      if (inputUsernameRef.current && inputBioRef.current) {
-        const inputUsername = inputUsernameRef.current;
-        const inputBio = inputBioRef.current;
+        if (inputUsernameRef.current && inputBioRef.current) {
+          const inputUsername = inputUsernameRef.current;
+          const inputBio = inputBioRef.current;
 
-        inputUsername.value = usernamenew;
-        inputBio.value = bionew;
+          inputUsername.value = usernamenew;
+          inputBio.value = bionew;
 
-        inputUsername.addEventListener('mouseover', () => {
-          if (inputUsername.value === usernamenew) {
+          inputUsername.addEventListener('mouseover', () => {
+            if (inputUsername.value === usernamenew) {
+              inputUsername.value = '';
+            }
+          });
+      
+          inputBio.addEventListener('mouseover', () => {
+            if (inputBio.value === bionew) {
+              inputBio.value = '';
+            }
+          });
+      
+          inputUsername.addEventListener('mouseout', () => {
+            if (inputUsername.value === '') {
+              inputUsername.value = usernamenew;
+            }
+          });
+      
+          inputBio.addEventListener('mouseout', () => {
+            if (inputBio.value === '') {
+              inputBio.value = bionew;
+            }
+          });
+      
+          inputUsername.addEventListener('click', () => {
             inputUsername.value = '';
-          }
-        });
-    
-        inputBio.addEventListener('mouseover', () => {
-          if (inputBio.value === bionew) {
+          });
+      
+          inputBio.addEventListener('click', () => {
             inputBio.value = '';
-          }
-        });
-    
-        inputUsername.addEventListener('mouseout', () => {
-          if (inputUsername.value === '') {
-            inputUsername.value = usernamenew;
-          }
-        });
-    
-        inputBio.addEventListener('mouseout', () => {
-          if (inputBio.value === '') {
-            inputBio.value = bionew;
-          }
-        });
-    
-        inputUsername.addEventListener('click', () => {
-          inputUsername.value = '';
-        });
-    
-        inputBio.addEventListener('click', () => {
-          inputBio.value = '';
-        });
+          });
+        }
       }
 
   };
+
+  
 
   if (IsAccountDeleted) {
     // Render a message or redirect to a different page after account deletion
@@ -162,7 +239,7 @@ const Profile = () => {
 
               {/* Edit Profile Modal */}
               {isEditModalOpen && (
-                <div id="editModal" className="editmodal_container">
+                <div id="editModal" className="editmodal_container" style={{ display: isEditModalOpen ? 'block' : 'none' }}>
                   <div className="editmodal_content">
                     <form className="editProfile">
                       <span className="close" onClick={handleCloseModal}>
@@ -178,12 +255,12 @@ const Profile = () => {
                         <br />
                         <br />
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="username" required ref={inputUsernameRef}/>
-                        <br />
+                        <input type="text" id="username" name="username" defaultValue={username} required ref={inputUsernameRef}/>
+                        <br /> 
                         <br />
 
                         <label htmlFor="bio">Bio</label>
-                        <textarea id="bio" name="bio" rows="4" cols="50" required ref={inputBioRef}></textarea>
+                        <textarea id="bio" name="bio" defaultValue={bio} rows="4" cols="50" required ref={inputBioRef}></textarea>
                         <br />
                         <br />
                       </div>
@@ -196,24 +273,50 @@ const Profile = () => {
                 </div>
               )}
 
-              <div id="namearea" className="details_info" ref={divUsernameRef}>
-                {username}
-              </div>
-              <div className="details_title">Bio</div>
-              <div id="bio_details" className="details_info" ref={divBioRef}>
-                {bio}
+              <div className="actions_container">
+                <button type="button" id="delete_btn" onClick={handleDeleteAccount}>
+                  Delete Account
+                </button>
               </div>
             </div>
-          </div>
 
-          <div className="actions_container">
-            <button type="button" id="delete_btn" onClick={handleDeleteAccount}>
-              Delete Account
-            </button>
+            <div class="details_container">
+              <div class="row">
+                <div class="column_1">
+
+                  <p>Username </p><br/>
+                  <p>Email</p><br/>
+                  <p>Gender</p><br/>
+                  <p>Date of Birth</p><br/>
+                  <p>Date Joined</p><br/><br/>
+
+                </div>
+
+                <div class="column_2">
+
+                  <div id="namearea" ref={divUsernameRef}>Zheng Wu Bang</div><br/>
+
+                  <div id="emailarea">wbzheng@gmail.com</div><br/>
+
+                  <div id="genderarea">Male</div><br/>
+
+                  <div id="DOBarea">12/5/2000</div><br/>
+
+                  <div id="DJoinedarea">4/7/2019</div><br/>
+
+                </div>
+              </div>
+            </div>
+
+            <hr/>
+
+            <div class="bio_container">
+              <p id="bio_details" ref={divBioRef}>I like travelling.</p>
+            </div>
           </div>
         </div>
-      </div>
-  
+
+      </div>  
   );
       
 };
